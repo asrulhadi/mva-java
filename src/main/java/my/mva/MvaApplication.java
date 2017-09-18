45f4d4055020a468e841688c89019ca6f12cbfe5
Asrul Hadi <git@asrulhadi.net>
2017-09-18 12:49:42 +0800

    Resolve error building war. Fix import

--- src/main/java/my/mva/MvaApplication.java
+++ src/main/java/my/mva/MvaApplication.java
@@ -2,14 +2,15 @@ package my.mva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class MvaApplication extends SpringBootServletInitializer {
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(Application.class);
+    return application.sources(MvaApplication.class);
   }
 
 	public static void main(String[] args) {
